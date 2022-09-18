8ecff2f0a4d53e8d4d3df287e919c8052338fff5
Scott Wilde <scottpaulwilde@gmail.com>
2022-09-17 17:07:07 -0700

    consolidate effects

--- image-generation/mask.js
+++ image-generation/mask.js
@@ -1,4 +1,5 @@
 const Canvas = require('canvas');
+const fleckIt = require('./effects/flecks');
 
 function applyMask(canvasColor, canvasAlpha, center, margin, width) {
     // the mask
@@ -199,101 +200,6 @@ function quarterIt(canvas, color) {
 
 }
 
-
-
-function drawFleck(ctx, color, xCenter, yCenter, ratio) {
-
-    let sides = Math.floor(Math.random() * 3) + 3;
-    //let sides = 4;
-    let angleIncr = Math.PI * 2 / sides;
-
-    let range = 16 * ratio;
-    let xOffset = Math.random() * range - range/2;
-    let yOffset = Math.random() * range - range/2;
-
-    let sizeRange = 6 * ratio;
-    let minSize = 2 * ratio;
-
-    for (let n = 0; n < 3; n++) {
-        let angleOffset = Math.random() * Math.PI * 2;
-        ctx.beginPath();
-
-        for (let i = 0; i < sides; i++) {
-            let size = Math.random() * sizeRange + minSize;
-            let angle = (angleIncr * i + angleOffset) % (Math.PI * 2);
-            let x = Math.cos(angle) * (size/2) + xCenter + xOffset;
-            let y = Math.sin(angle) * (size/2) + yCenter + yOffset;
-            if (i === 0) ctx.moveTo(x, y);
-            else ctx.lineTo(x, y);
-        }
-        ctx.closePath();
-        ctx.fillStyle = alphaColor(color, 0.5); // "#" + color;
-        ctx.fill();
-    }
-
-}
-
-function fleckIt(canvas, color) {
-    let ctx = canvas.getContext('2d');
-    let colorArray = Array.isArray(color.flecked) ? color.flecked : [color.flecked];
-    let colorIndex = 0;
-
-    let radius = 1;
-    let angle = 0;
-    let maxRadius = Math.max(canvas.width, canvas.height);
-    let center = {x:canvas.width/2, y: canvas.height/2};
-
-    let ratio = canvas.height / (1200 * 3);
-
-    let idealDistance = 15 * ratio;
-    let radiusIncrPerRevolution = 12 * ratio;
-
-    let maxTimes = 1000000;
-
-    while (radius < maxRadius && maxTimes > 0) {
-        // find point
-        let x = Math.cos(angle) * radius + center.x;
-        let y = Math.sin(angle) * radius + center.y;
-
-        // draw at point
-        drawFleck(ctx, colorArray[colorIndex++ % colorArray.length], x, y, ratio);
-
-
-        let d = idealDistance;
-        let percent = -1;
-
-        while (d > 0) {
-            // determine circumference
-            let c = Math.PI * 2 * radius;
-
-            if (d > c) {
-                let percent = 0.01;
-                radius += radiusIncrPerRevolution * percent;
-                angle += Math.PI * 2 * percent;
-
-                c = Math.PI * 2 * radius * percent;
-
-                d -= c;
-
-                // angle stays the same
-            }
-            else {
-
-                percent = d / c;
-                angle += Math.PI * 2 * percent;
-                angle = angle % (Math.PI * 2);
-                radius += radiusIncrPerRevolution * percent;
-
-                d = 0; //exit
-            }
-        }
-
-        //console.log(maxTimes + " radius:" + radius + " percent:" + percent + " circum:" + c);
-
-        maxTimes--;
-    }
-
-}
 
 // square circles
 function drawCircles(canvas, color, layerSize) {
